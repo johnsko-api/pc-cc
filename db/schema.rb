82205246b17bf41a8ab4eb82cb6768600fff7949
chanhpark <chanhyun.park1211@gmail.com>
2015-02-26 17:08:14 -0500

    regions table added with the team association

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226210819) do
+ActiveRecord::Schema.define(version: 20150226220427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "regions", force: :cascade do |t|
+    t.string "name", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
